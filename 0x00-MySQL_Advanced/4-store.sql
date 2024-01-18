76b4192ee532ba87d9a933055ebd83d148786f55
Blxee <aminedev57@gmail.com>
2024-01-18 19:55:30 +0100

    Added: task (4. Buy buy buy) for 0x00 MySQL Advanced

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -1,2 +1,12 @@
 -- 4. Buy buy buy
-
+DELIMITER //
+CREATE TRIGGER decrease_trigger
+AFTER INSERT
+ON orders
+FOR EACH ROW
+BEGIN
+  UPDATE items
+  SET quantity = quantity - NEW.number
+  WHERE name = NEW.item_name;
+END;//
+DELIMITER ;
