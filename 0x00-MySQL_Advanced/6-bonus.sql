dddba924bbc6f062c965589d517dd0fd231b31bc
Blxee <aminedev57@gmail.com>
2024-01-18 22:49:57 +0100

    Added: task (6. Add bonus) for 0x00 MySQL Advanced

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -8,7 +8,10 @@ CREATE PROCEDURE AddBonus (
 BEGIN
   DECLARE project_id INT;
   SELECT id INTO project_id FROM projects WHERE name = project_name;
-
+  IF project_id IS NULL THEN
+    INSERT INTO projects(name) VALUES(project_name);
+    SET project_id = LAST_INSERT_ID();
+  END IF;
   INSERT INTO corrections VALUES(user_id, project_id, score);
 END
 DELIMITER ;
