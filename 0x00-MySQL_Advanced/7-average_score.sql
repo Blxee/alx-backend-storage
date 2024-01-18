43bd147b5e0433e8cc1d03a8c8d024cb652f1371
Blxee <aminedev57@gmail.com>
2024-01-18 23:07:43 +0100

    Added: task (7. Average score) for 0x00 MySQL Advanced

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -6,7 +6,7 @@ BEGIN
   SELECT AVG(score) INTO average
     FROM corrections
     GROUP BY user_id
-    WHERE user_id = user_id;
+    WHERE corrections.user_id = user_id;
   UPDATE users
     SET average_score = average
     WHERE id = user_id;
