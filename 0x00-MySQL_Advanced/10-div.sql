02882331341f737ce125242c89261c59e330b025
Blxee <aminedev57@gmail.com>
2024-01-19 02:44:01 +0100

    Added: task (10. safe divide) for 0x00 MySQL Advanced

--- 0x00-MySQL_Advanced/10-div.sql
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -1,4 +1,13 @@
 -- 10. Safe divide
+DELIMITER //
 CREATE FUNCTION SafeDiv (a INT, b INT)
 RETURNS FLOAT
-RETURN COALESCE(a / b, 0);
+BEGIN
+  DECLARE result FLOAT;
+  SET result = a / b;
+  IF result IS NULL THEN
+    SET result = 0;
+  END IF;
+  RETURN result;
+END;//
+DELIMITER ;
