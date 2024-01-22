d592cbed04d8b4fc8236065b200007320f990eab
Blxee <aminedev57@gmail.com>
2024-01-22 13:46:37 +0100

    Added: task (9. Insert a document in python) of 0x01. NoSQL

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -1,2 +1,16 @@
 #!/usr/bin/env python3
-9. Insert a document in Python
+"""9. Insert a document in Python"""
+
+
+def insert_school(mongo_collection, **kwargs):
+    """Inserts a new document into a collection based on kwargs.
+
+    Args:
+        mongo_collection: the target collection.
+    Kwargs:
+        the fields of the new document.
+    Returns:
+        the field id.
+    """
+    document = mongo_collection.insert_one(kwargs)
+    return document.inserted_id
