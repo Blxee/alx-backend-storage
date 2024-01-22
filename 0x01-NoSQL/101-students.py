87ce3842a55852024f220ba78c43dd99be3ad84b
Blxee <aminedev57@gmail.com>
2024-01-22 15:38:42 +0100

    Added: task (14. Top students) of 0x01. NoSQL

--- 0x01-NoSQL/101-students.py
+++ 0x01-NoSQL/101-students.py
@@ -1,2 +1,13 @@
 #!/usr/bin/env python3
-14. Top students
+"""14. Top students"""
+
+
+def top_students(mongo_collection):
+    """Fetches all students from the collection sorted by average score.
+
+    Args:
+        mongo_collection: the target collection.
+    Returns:
+       list: the resulted list.
+    """
+    return list(mongo_collection.find().sort({'averageScore': -1}))
