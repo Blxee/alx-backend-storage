9b2123cccf8f2b7eec0de038edb58e24c69325cf
Blxee <aminedev57@gmail.com>
2024-01-22 14:54:12 +0100

    Added: task (11. Where can I learn Python? ) of 0x01. NoSQL

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -1,2 +1,14 @@
 #!/usr/bin/env python3
-11. Where can I learn Python?
+"""11. Where can I learn Python?"""
+
+
+def schools_by_topic(mongo_collection, topic):
+    """Retrieves list of schools which have a specific topic.
+
+    Args:
+        mongo_collection: the target collection.
+        topic: the topic to filter by.
+    Returns:
+        list: a list of schools.
+    """
+    list(mongo_collection.find({'topics': topic}))
