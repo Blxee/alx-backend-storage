8c846475b094c0cbfef0a9ab81c1314a46555c36
Blxee <aminedev57@gmail.com>
2024-01-22 13:13:01 +0100

    Added: task (8. List all documents in python) of 0x01. NoSQL

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,2 +1,14 @@
 #!/usr/bin/env python3
-8. List all documents in Python
+"""8. List all documents in Python"""
+
+
+def list_all(mongo_collection):
+    """Lists all documents in a mongodb collection.
+
+    Args:
+        mongo_collection: the target collection.
+
+    Returns:
+        list: all the documents in the collection in a list format.
+    """
+    return mongo_collection.find()
