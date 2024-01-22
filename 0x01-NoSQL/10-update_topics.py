011b9e5beac33e605f576bbcf1b1ed5cf8138678
Blxee <aminedev57@gmail.com>
2024-01-22 14:21:12 +0100

    Added: task (10. Change school topics) of 0x01. NoSQL

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -1,2 +1,13 @@
 #!/usr/bin/env python3
-10. Change school topics
+"""10. Change school topics"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """Updates all school document with new topics in a collection.
+
+    Args:
+        mongo_collection: the target collection.
+        name: the name of the schools to use in filter.
+        topics: the new topics that the schools shuold be updated with.
+    """
+    mongo_collection.update_many({'name': name}, {'topics': topics})
