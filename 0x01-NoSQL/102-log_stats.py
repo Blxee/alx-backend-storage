e63e8a0cc27c4cd3db0e6db1b0faa660c8541297
Blxee <aminedev57@gmail.com>
2024-01-22 16:56:50 +0100

    Added: task (15. Log stats - new version) of 0x01. NoSQL

--- 0x01-NoSQL/102-log_stats.py
+++ 0x01-NoSQL/102-log_stats.py
@@ -1,2 +1,18 @@
 #!/usr/bin/env python3
-15. Log stats - new version
+"""15. Log stats - new version"""
+
+
+if __name__ == '__main__':
+    import pymongo
+    client = pymongo.MongoClient('mongodb://127.0.0.1:27017')
+    coll = client.logs.nginx
+    print(coll.count_documents({}), 'logs')
+    print('Methods:')
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    for method in methods:
+        print(f'\tmethod {method}:', coll.count_documents({'method': method}))
+    print(coll.count_documents({'method': 'GET', 'path': '/status'}))
+    print('IPs:')
+    most_present = coll.aggregate([{'$group': {'_id': '$ip', 'occurence': {'$count': {}}}}]).sort({'occurence': -1})
+    for item in most_present:
+        print(f'\t{item["_id"]}: {item["occurence"]}')
