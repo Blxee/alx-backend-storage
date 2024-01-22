12bdfe8fd022a371bc956879405f9eca54ae60e3
Blxee <aminedev57@gmail.com>
2024-01-22 15:22:06 +0100

    Added: task (12. Log stats) of 0x01. NoSQL

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,2 +1,14 @@
 #!/usr/bin/env python3
-12. Log stats
+"""12. Log stats"""
+
+
+if __name__ == '__main__':
+    import pymongo
+    client = pymongo.MongoClient('mongodb://127.0.0.1:27017')
+    coll = client.logs.nginx
+    print(coll.count_documents(), 'logs')
+    print('Methods:')
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    for method in methods:
+        print(f'\tmethod {method}:', coll.count_documents({'method': method}))
+    print(coll.count_documents({'method': 'GET', 'path': '/status'}))
