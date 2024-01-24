33bed58fe9b33a963f8affd3ce3dbddbebd6577e
Blxee <aminedev57@gmail.com>
2024-01-24 11:52:13 +0100

    Added: task (1. Reading from Redis and recovering original type) of 0x02. Redis basic

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """Main module for all mandatory tasks."""
 from redis import Redis
-from typing import Any
+from typing import Any, Callable
 from uuid import uuid4
 
 
@@ -17,3 +17,10 @@ class Cache:
         key = str(uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Callable | None = None) -> bytes | None:
+        """Retrieves data from redis using a key, and a convertion function."""
+        if fn:
+            return fn(self._redis.get(key))
+        else:
+            return self._redis.get(key)
