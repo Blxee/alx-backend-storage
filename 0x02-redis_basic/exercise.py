1ddca6ece7e2c3d4cffb59a94c1611a515740649
Blxee <aminedev57@gmail.com>
2024-01-24 12:02:44 +0100

    Revert "Fixed: task (1. Reading from Redis and recovering original type) of 0x02. Redis basic"
    This reverts commit 7ad732950400dc64d7cb30dcb9a0836597de43ed.

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -7,28 +7,28 @@ from uuid import uuid4
 
 class Cache:
     """Cache class, a client which connects to reddis"""
-    def __init__(self: 'Cache') -> None:
+    def __init__(self) -> None:
         """Constructor for Cache class."""
         self._redis = Redis()
         self._redis.flushdb()
 
-    def store(self: 'Cache', data: Any) -> str:
+    def store(self, data: Any) -> str:
         """Stores data in redis using a key."""
         key = str(uuid4())
         self._redis.set(key, data)
         return key
 
-    def get(self: 'Cache', key: str, fn: Callable | None = None) -> Any | None:
+    def get(self, key: str, fn: Callable | None = None) -> Any | None:
         """Retrieves data from redis using a key, and a convertion function."""
         if fn:
             return fn(self._redis.get(key))
         else:
             return self._redis.get(key)
 
-    def get_str(self: 'Cache', key: str) -> str | None:
+    def get_str(self, key: str) -> str | None:
         "Retrieves a string from reddis"
         return self.get(key, str)
 
-    def get_int(self: 'Cache', key: str) -> int | None:
+    def get_int(self, key: str) -> int | None:
         "Retrieves a int from reddis"
         return self.get(key, int)
