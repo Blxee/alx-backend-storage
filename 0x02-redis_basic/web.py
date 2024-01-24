933a1783f545e34f644e242771a6ade5a74f757b
Blxee <aminedev57@gmail.com>
2024-01-24 22:11:03 +0100

    Added: task (5. Implementing an expiring web cache and tracker) of 0x02 Redis basic

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -8,6 +8,7 @@ from typing import Callable, Any
 
 def count_access(fn: Callable) -> Callable:
     redis = Redis()
+
     @wraps(fn)
     def wrapper(url, *args, **kwargs) -> Any:
         key = 'count:' + url
