244f1d94b941cdd96d0e6a6fe6ae7708a87e386c
Blxee <aminedev57@gmail.com>
2024-01-24 22:10:04 +0100

    Added: task (5. Implementing an expiring web cache and tracker) of 0x02 Redis basic

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,8 +1,23 @@
 #!/usr/bin/env python3
 """5. Implementing an expiring web cache and tracker."""
+from functools import wraps
+from redis import Redis
 import requests
+from typing import Callable, Any
 
 
+def count_access(fn: Callable) -> Callable:
+    redis = Redis()
+    @wraps(fn)
+    def wrapper(url, *args, **kwargs) -> Any:
+        key = 'count:' + url
+        redis.incr(key)
+        redis.expire(key, 10)
+        return fn(url, *args, **kwargs)
+    return wrapper
+
+
+@count_access
 def get_page(url: str) -> str:
     """Retrieves the content of an html page."""
     return requests.get(url, timeout=10).text
